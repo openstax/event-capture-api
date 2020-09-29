d65a37b7b01d61f0be72681183783e83381ff7a6
JP Slavinsky <jpslav@gmail.com>
2020-09-29 16:35:46 -0600

    common schema types abstracted (#18)
    * common schema types abstracted
    
    * more targeted

--- schemas/org/openstax/ec/nudged/v1/avro_builder.rb
+++ schemas/org/openstax/ec/nudged/v1/avro_builder.rb
@@ -1,7 +1,6 @@
 namespace 'org.openstax.ec'
 
-# TODO put this and other common types in a shared / imported file
-fixed :uuid, 16
+import 'types/uuid'
 
 record :nudged_v1 do
   required :user_uuid, :uuid
