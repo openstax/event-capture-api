02d37f03826682ba0bc1e2588004e0b4264f4e06
Steve Burkett <steve@burkettnet.com>
2020-10-20 08:44:39 -0700

    Renamed to past tense

--- schemas/org/openstax/ec/accessed_studyguide/v1/avro_builder.rb
+++ schemas/org/openstax/ec/accessed_studyguide/v1/avro_builder.rb
@@ -3,7 +3,7 @@ namespace 'org.openstax.ec'
 import 'types/uuid'
 import 'types/date_time'
 
-record :access_studyguide_v1 do
+record :accessed_studyguide_v1 do
   required :user_uuid, :uuid
   required :page_id, :string
   required :occurred_at, :timestamp
--- schemas/org/openstax/ec/created_highlight/v1/avro_builder.rb
+++ schemas/org/openstax/ec/created_highlight/v1/avro_builder.rb
@@ -3,7 +3,7 @@ namespace 'org.openstax.ec'
 import 'types/uuid'
 import 'types/date_time'
 
-record :create_highlight_v1 do
+record :created_highlight_v1 do
   required :user_uuid, :uuid
   required :contents, :string
   required :location, :string
