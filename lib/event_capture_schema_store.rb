823840c7abfb6ed580e467db68de9a5a75f8cd95
Steve Burkett <steve@burkettnet.com>
2020-10-31 10:08:48 -0700

    Improve codecov

--- lib/event_capture_schema_store.rb
+++ lib/event_capture_schema_store.rb
@@ -5,6 +5,6 @@ class EventCaptureSchemaStore < AvroTurf::SchemaStore
   def build_schema_path(fullname)
     *namespace, schema_name = fullname.split(".")
     schema_name_without_version, version = schema_name.split(/_(v\d+)\z/)
-    schema_path = File.join(@path, *namespace, schema_name_without_version, version, "avro.avsc")
+    File.join(@path, *namespace, schema_name_without_version, version, 'avro.avsc')
   end
 end
