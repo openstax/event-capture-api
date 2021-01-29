77566667524a679fdf3061395e317ea62d66dcc9
Ross Reedstrom <reedstrm@gmail.com>
2021-01-28 21:59:37 -0600

    Convert to subclassing hash. Idiomatic style cleanup in spec

--- app/controllers/api/v0/events_controller.rb
+++ app/controllers/api/v0/events_controller.rb
@@ -6,7 +6,7 @@ class Api::V0::EventsController < Api::V0::BaseController
     render(json: error, status: error.status_code) and return if error
 
     inbound_binding.events.each do |event|
-      raw_kafka_data = KafkaData.new(api_data: event.data, controller: self).to_hash
+      raw_kafka_data = KafkaData.new(api_data: event.data, controller: self)
       avro_kafka_data = KafkaAvroTurf.instance.encode(raw_kafka_data, schema_name: event.data.type)
 
       kafka_topic = TopicsConfig.get_topic_for_event(event)
@@ -16,9 +16,10 @@ class Api::V0::EventsController < Api::V0::BaseController
     render nothing: true, status: 201
   end
 
-  class KafkaData
+  class KafkaData < Hash
     def initialize(api_data:, controller:)
-      @data = api_data.to_hash.deep_dup
+      merge!(api_data)
+
       @controller = controller
 
       translate_started_session
@@ -27,54 +28,50 @@ class Api::V0::EventsController < Api::V0::BaseController
       fixup_timestamps
     end
 
-    def to_hash
-      data
-    end
-
     protected
 
     attr_reader :data, :controller
 
     def translate_started_session
       # Capture request-level data for session starts
-      return unless data[:type].include?('org.openstax.ec.started_session')
-      data[:ip_address] = controller.request.remote_ip
-      data[:user_agent] = controller.request.headers['User-Agent']
+      return unless self[:type].include?('org.openstax.ec.started_session')
+
+      self[:ip_address] = controller.request.remote_ip
+      self[:user_agent] = controller.request.headers['User-Agent']
     end
 
     def translate_source_uri
       # Split source_uri into parts
       # URI translates anything other than nil, and the swagger schema requires
-      # source_uri to be non-nil
-      # TODO parse query params into a hash of some sort?
-      
-      source_uri = URI(data[:source_uri])
+      # source_uri to be non-nil - query might be nil, though
 
-      data[:scheme] = source_uri.scheme
-      data[:host] = source_uri.host
-      data[:path] = source_uri.path
-      data[:query] = source_uri.query ? CGI.parse(source_uri.query) : nil
+      source_uri = URI(delete(:source_uri))
 
-      data.except!(:source_uri)
+      merge!({
+               scheme: source_uri.scheme,
+               host: source_uri.host,
+               path: source_uri.path,
+               query: source_uri.query ? CGI.parse(source_uri.query) : nil
+             })
     end
 
     def translate_uuids
       # Set the user uuid according to the currently logged in user
-      data[:user_uuid] = CompactUuid.pack(controller.current_user_uuid) if controller.current_user_uuid
-      data[:device_uuid] = CompactUuid.pack(controller.current_device_uuid) if controller.current_device_uuid
-      data[:session_uuid] = CompactUuid.pack(data[:session_uuid])
+      self[:user_uuid] = CompactUuid.pack(controller.current_user_uuid) if controller.current_user_uuid
+      self[:device_uuid] = CompactUuid.pack(controller.current_device_uuid) if controller.current_device_uuid
+      self[:session_uuid] = CompactUuid.pack(self[:session_uuid])
     end
 
     def fixup_timestamps
       # Adjust the client's occurred at time by a device sent at adjustment
-      data[:occurred_at] = TimeUtil.infer_actual_occurred_at_from_client_timestamps(
-        request_received_at: controller.request.env[:received_at],
-        client_clock_occurred_at: data[:client_clock_occurred_at],
-        client_clock_sent_at: data[:client_clock_sent_at]
-      ).to_i
+      sent_at = delete(:client_clock_sent_at)
+      occurred_at = delete(:client_clock_occurred_at)
 
-      data.except!(:client_clock_sent_at, :client_clock_occurred_at)
+      self[:occurred_at] = TimeUtil.infer_actual_occurred_at_from_client_timestamps(
+        request_received_at: controller.request.env[:received_at],
+        client_clock_occurred_at: occurred_at,
+        client_clock_sent_at: sent_at
+      ).to_i
     end
   end
 end
-
---
+++ spec/requests/api/v0/kafka_data_spec.rb
@@ -0,0 +1,98 @@
+# frozen_string_literal: true
+
+require 'ostruct'
+
+RSpec.describe Api::V0::EventsController::KafkaData do
+  # mock controller and request
+  let(:remote_ip) { '192.168.1.1' }
+  let(:user_id) { 'd8388680-80cf-4fdb-95bb-829a46342115' }
+  let(:received_at) { Time.parse('2020-10-06T18:14:27Z') }
+
+  let(:fake_controller) do
+    OpenStruct.new(
+      request: OpenStruct.new(
+        remote_ip: remote_ip,
+        env: OpenStruct.new(
+          received_at: received_at
+        ),
+        headers: OpenStruct.new(
+          'User-Agent' => 'My Crazy Test Browser'
+        )
+      ),
+      current_user_uuid: user_id
+    )
+  end
+
+  # posted data
+  let(:session_uuid) { 'ed3cdd93-6688-4fcb-b5b3-da3e71052454' }
+  let(:client_clock_occurred_at) { '2020-10-06T18:14:22Z' }
+  let(:client_clock_sent_at) { '2020-10-06T18:14:22Z' }
+
+  let(:api_data) do
+    {
+      "client_clock_occurred_at": client_clock_occurred_at,
+      "client_clock_sent_at": client_clock_sent_at,
+      'type': 'org.openstax.ec.started_session',
+      'referrer': 'https://lms.example.com/some/other/path',
+      'source_uri': 'https://rex.example.com/some/book/id?book_id=34343&words=me&words=and&words=my%20shadow',
+      'session_uuid': session_uuid
+    }
+  end
+
+  let(:instance) { described_class.new(api_data: api_data, controller: fake_controller).to_hash }
+
+  # Request header captures
+  it 'captures the remote IP from request headers' do
+    expect(instance).to include({ ip_address: remote_ip })
+  end
+  it 'captures the referrer from request headers' do
+    expect(instance).to include({ referrer: 'https://lms.example.com/some/other/path' })
+  end
+  it 'catures the user_agent from the request headers' do
+    expect(instance).to include({ user_agent: 'My Crazy Test Browser' })
+  end
+
+  # Event time calculation
+  let(:occured_at) do
+    TimeUtil.infer_actual_occurred_at_from_client_timestamps(
+      request_received_at: received_at,
+      client_clock_occurred_at: client_clock_occurred_at,
+      client_clock_sent_at: client_clock_sent_at
+    ).to_i
+  end
+  it 'calculates the occured_at timestamp' do
+    expect(instance).to include({ occurred_at: occured_at })
+  end
+
+  # Source_uri parsing
+  it 'extracts the source_uri schema' do
+    expect(instance).to include({ scheme: 'https' })
+  end
+  it 'extracts host from source_uri' do
+    expect(instance).to include({ host: 'rex.example.com' })
+  end
+  it 'extracts the path from source_uri' do
+    expect(instance).to include({ path: '/some/book/id' })
+  end
+  it 'extracts and parses the query from source_uri' do
+    expect(instance).to include(
+      { query: { 'book_id' => ['34343'], 'words' => ['me', 'and', 'my shadow'] } }
+    )
+  end
+
+  # uuid handling
+  it 'compacts the session_id' do
+    expect(instance).to include(
+      { session_uuid: CompactUuid.pack(session_uuid) }
+    )
+  end
+  it 'compacts  the user_id' do
+    expect(instance).to include(
+      { user_uuid: CompactUuid.pack(user_id) }
+    )
+  end
+
+  it 'includes the event type' do
+    expect(instance).to include({ type: 'org.openstax.ec.started_session' })
+  end
+end
