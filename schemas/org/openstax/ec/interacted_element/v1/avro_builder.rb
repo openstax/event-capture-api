7bbb8f500f21b7a47a039cdb52fb26f85e5468fa
Ross Reedstrom <reedstrm@gmail.com>
2022-10-21 11:45:28 -0500

    first cut interaction even schema

---
+++ schemas/org/openstax/ec/interacted_element/v1/avro_builder.rb
@@ -0,0 +1,19 @@
+namespace 'org.openstax.ec'
+
+import 'types/uuid'
+import 'types/date_time'
+import 'types/base_web_event'
+
+record :interacted_element_v1 do
+  extends :base_web_event
+  required :target_id, :string
+  required :target_type, :string
+  required :target_attributes, :map, :values :string
+  required :context_id, :string
+  required :context_type, :string
+  required :context_attributes, :map, :values :string
+  required :occurred_at, :timestamp
+
+end
+
+
