4dc178d8551995f6cd716b7173e13f974a2d7f44
Ross Reedstrom <reedstrm@gmail.com>
2023-02-16 14:32:54 -0600

    New event: changed_state (visibility or other types) (#69)
    * make docker ruby version match google actions
    
    * First cut changed_session event
    
    * fix yaml syntax
    
    * Rename event to changed_state, allow for multiple types of state
    
    ---------
    
    Co-authored-by: staxly[bot] <35789409+staxly[bot]@users.noreply.github.com>

---
+++ schemas/org/openstax/ec/changed_state/v1/avro_builder.rb
@@ -0,0 +1,14 @@
+namespace 'org.openstax.ec'
+
+import 'types/uuid'
+import 'types/date_time'
+import 'types/base_web_event'
+
+record :interacted_element_v1 do
+  extends :base_web_event
+  required :type, :string
+  required :current, :string
+  required :previous, :string
+end
+
+
